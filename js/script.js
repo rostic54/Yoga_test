377865899e2b178b9600c02a9e50bf30d4eb59d1
rostic54 <rostic54@gmail.com>
2017-11-14 14:03:41 +0200

    markup almost ready, without little slider and Burger menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 "use strict";
 
 function mason(){
-    $('.grid').masonry({
+    $( '.grid' ).masonry({
         itemSelector: '.grid__item',
         columnWidth: '.grid__item',
     })
 };
 
-function slider(){
-    $('.reviews').slick({
+function sliderComments(){
+    $( '.reviews' ).slick({
         vertical: true,
         dots: true,
         dotsClass: 'dots-coment',
@@ -17,8 +17,29 @@ function slider(){
     });
 };
 
+function sliderLittle(){
+    $('.teacher').slick({
+        prevArrow: '<a href="" class="mid-slide__prev mid-slide__btn"><</a>',
+        nextArrow: '<a href="" class="mid-slide__next mid-slide__btn">></a>'
+    
+    });
+}
+
+function sliderMiddle(){
+    $( '.mid-slide' ).slick({
+        prevArrow: '<a href="" class="mid-slide__prev mid-slide__btn"><</a>',
+        nextArrow: '<a href="" class="mid-slide__next mid-slide__btn">></a>'
+    
+        /* nextArrow:'<img src="../img/next.png">',
+         prevArrow:'<img src="../img/prev.png">'*/
+    });
+
+}
+
 
 $( document ).ready( function(){
     mason();
-    slider();
+    sliderComments();
+    sliderMiddle();
+    sliderLittle();
 })
